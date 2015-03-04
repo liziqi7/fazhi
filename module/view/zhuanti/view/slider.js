c27d952a24fb4ebfca824704c7c085fed37c06b7
liziqi7 <liziqi7@gmail.com>
2015-03-04 20:04:25 +0800

    修改

--- module/view/zhuanti/view/slider.js
+++ module/view/zhuanti/view/slider.js
@@ -2,28 +2,36 @@ define('', '', function(require) {
 	var B = require('backbone');
 	var M = require('base/model');
 	var Swipe = require('plusin/swipe');
-	var H = require('text!../../../../tpl/index/view/slider.html');
+	var H = require('text!../../../../tpl/zhuanti/view/slider.html');
 	var model = new M();
 	var V = B.View.extend({
 		model: model,
 		template: H,
 		initialize: function() {
-			var t = this;			
+			var t = this;
 			// t.listenTo(t.model, "sync", function() {
-				t.render();
+			t.render();
 			// });			
 		},
 		//待优化
 		render: function() {
 			var t = this,
-				data = {"data":[{"id":0,"pic":"resource/images/s1.png"},{"id":1,"pic":"resource/images/s2.png"}]};//t.model.toJSON();
+				data = {
+					"data": [{
+						"id": 0,
+						"pic": "resource/images/s1.png"
+					}, {
+						"id": 1,
+						"pic": "resource/images/s2.png"
+					}]
+				}; //t.model.toJSON();
 			var html = _.template(t.template, data);
 			t.$el.show().html(html);
 			t.doSlider();
-			Jser.loadimages(t.$el);			
+			Jser.loadimages(t.$el);
 		},
 		doSlider: function() {
-			var t=this;
+			var t = this;
 			window.global_indexSwipe = Swipe(t.$el.find(".slider-box")[0], {
 				stopPropagation: true,
 				continuous: true,
